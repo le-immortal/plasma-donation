7c9d44a8320f8516e8c13006e5bc049b8ee96995
Abhinav-li <abhinav.sharma10@learner.manipal.edu>
2021-05-05 01:34:21 +0530

    first commit

---
+++ assets/page_control.js
@@ -0,0 +1,30 @@
+function goPatient(){
+    window.location = './pages/Patientform.html';
+}
+
+function goDonor(){
+    window.location = './pages/Donorform.html';
+}
+
+var buffer = 20; //scroll bar buffer
+var iframe = document.getElementById('ifm');
+
+function pageY(elem) {
+    return elem.offsetParent ? (elem.offsetTop + pageY(elem.offsetParent)) : elem.offsetTop;
+}
+
+function resizeIframe() {
+    var height = document.documentElement.clientHeight;
+    height -= pageY(document.getElementById('ifm'))+ buffer ;
+    height = (height < 0) ? 0 : height;
+    document.getElementById('ifm').style.height = height + 'px';
+}
+
+// .onload doesn't work with IE8 and older.
+if (iframe.attachEvent) {
+    iframe.attachEvent("onload", resizeIframe);
+} else {
+    iframe.onload=resizeIframe;
+}
+
+window.onresize = resizeIframe;
